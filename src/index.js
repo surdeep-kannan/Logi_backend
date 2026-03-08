75160bb55a50da94f9b7e58089dba16b801b9d8c
surdeep-kannan <surdeepkannan2124@email.com>
2026-03-08 13:27:27 +0530

    fix: add company frontend URLs to CORS

--- src/index.js
+++ src/index.js
@@ -26,10 +26,8 @@ app.use(helmet())
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
-    "http://localhost:5173",
-    "http://localhost:3000",
+    "https://logi-company-r55jleuk1-surdeep-kannans-projects.vercel.app",
     "https://logisticnowhack.vercel.app",
-    "http://localhost:5173/"
   ],
   credentials: true,
 }))
